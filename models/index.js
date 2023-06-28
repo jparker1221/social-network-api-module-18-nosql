8554be35c5486d22d74bed31c990bf5d98f45530
jparker1221 <jonparker1221@gmail.com>
2023-06-28 17:01:10 -0400

    added basic file structure and boilerplate code

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Import User and Thought models
+const User = require('./User');
+const Thought = require('./Thought');
+
+// Export User and Thought models for use elsewhere in the application
+module.exports = { User, Thought };
